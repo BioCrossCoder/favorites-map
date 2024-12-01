d96965301c7c3f7c76bc99fb7f6e844895459dbc
BioCrossCoder <viscaria_dignics@163.com>
2024-12-01 22:00:04 +0800

    [fix] correct interface file path

--- entrypoints/background.ts
+++ entrypoints/background.ts
@@ -1,27 +1,44 @@
-import { Node, IndexNode, DataNode, Action, OperationMessage, SearchResultMessage } from "./interface";
+import { Node, IndexNode, DataNode, Action, OperationMessage, SearchResultMessage } from "../interface";
 
 type GraphData = {
-  dataNodes: Map<string, Node>;
+  dataNodes: Map<string, DataNode>;
   nodes: Map<string, Node>;
+};
+const storageKey: `local:${string}` = 'local:favorites_map';
+let data: GraphData = {
+  dataNodes: new Map<string, DataNode>(),
+  nodes: new Map<string, Node>(),
+}
+
+class GraphStorage {
+  public static async load(): Promise<void> {
+    data = await storage.getMeta<GraphData>(storageKey);
+  }
+  public static async dump(): Promise<void> {
+    await storage.setMeta(storageKey, data);
+  }
+  private static _monitor() {
+    storage.watch(storageKey, GraphStorage.load);
+  }
+  public static syncStorageData() {
+    storage.getItem(storageKey).then((value) => {
+      if (!value) {
+        GraphStorage.dump().then(GraphStorage._monitor);
+      } else {
+        GraphStorage._monitor();
+      }
+    })
+  }
 };
 
 class Graph {
   // [Singleton]
   private constructor() {
-    storage.watch(Graph._storageKey, this.load);
+    GraphStorage.syncStorageData();
   }
   private static _graph = new Graph();
   public static get instance(): Graph {
     return Graph._graph;
-  } // [/]
-  // [DataStorage]
-  private static _storageKey: `local:${string}` = 'local:favorites_map';
-  private _data = {} as GraphData;
-  public async load(): Promise<void> {
-    this._data = await storage.getMeta<GraphData>(Graph._storageKey);
-  }
-  public async dump(): Promise<void> {
-    await storage.setMeta(Graph._storageKey, this._data);
   } // [/]
   private updateRelations(oldNode: Node, newNode: Node): void {
     // [CheckID]
@@ -30,50 +47,50 @@ class Graph {
     } // [/]
     // [RemoveInvalidRelations]
     for (const name of oldNode.relatedNodeNames.difference(newNode.relatedNodeNames)) {
-      const relatedNode: Node | undefined = this._data.nodes.get(name);
+      const relatedNode: Node | undefined = data.nodes.get(name);
       if (relatedNode) {
         relatedNode.relatedNodeNames.delete(oldNode.name);
       }
     } // [/]
     // [AddNewRelations]
     for (const name of newNode.relatedNodeNames.difference(oldNode.relatedNodeNames)) {
-      const relatedNode: Node | undefined = this._data.nodes.get(name);
+      const relatedNode: Node | undefined = data.nodes.get(name);
       if (relatedNode) {
         relatedNode.relatedNodeNames.add(newNode.name);
       }
     } // [/]
   }
   public upsert(node: Node): void {
-    const currentNode: Node | undefined = this._data.nodes.get(node.name);
+    const currentNode: Node | undefined = data.nodes.get(node.name);
     if (currentNode) {
       this.updateRelations(currentNode, node);
     }
-    this._data.nodes.set(node.name, node);
+    data.nodes.set(node.name, node);
     if (node instanceof DataNode) {
-      this._data.dataNodes.set(node.url, node);
+      data.dataNodes.set(node.url, node);
     }
-    this.dump();
+    GraphStorage.dump();
   }
   public delete(name: string): void {
-    const node: Node | undefined = this._data.nodes.get(name);
+    const node: Node | undefined = data.nodes.get(name);
     if (!node) {
       return
     }
     this.updateRelations(node, new Node(node.name, new Set<string>()));
-    this._data.nodes.delete(node.name);
+    data.nodes.delete(node.name);
     if (node instanceof DataNode) {
-      this._data.dataNodes.delete(node.url);
+      data.dataNodes.delete(node.url);
     }
-    this.dump();
+    GraphStorage.dump();
   }
   public search(keyword: string): Set<Node> {
     const result = new Set<Node>();
-    if (!this._data) {
+    if (!data) {
       return result;
     }
     const indexNodes = new Set<IndexNode>();
     // [SearchByName]
-    for (const [name, node] of this._data.nodes) {
+    for (const [name, node] of data.nodes) {
       if (!keyword || name.includes(keyword)) {
         if (node instanceof DataNode) {
           result.add(node);
@@ -83,7 +100,7 @@ class Graph {
       }
     } // [/]
     // [SearchByURL]
-    for (const [url, node] of this._data.dataNodes) {
+    for (const [url, node] of data.dataNodes) {
       if (!keyword || url.toLowerCase().includes(keyword.toLowerCase())) {
         result.add(node);
       }
@@ -93,7 +110,7 @@ class Graph {
 }
 
 export default defineBackground(() => {
-  browser.runtime.onInstalled.addListener(Graph.instance.load);
+  browser.runtime.onInstalled.addListener(GraphStorage.load);
   browser.runtime.onMessage.addListener((message: OperationMessage, _sender, sendResponse: (response: SearchResultMessage) => void) => {
     switch (message.action) {
       case Action.Upsert:
