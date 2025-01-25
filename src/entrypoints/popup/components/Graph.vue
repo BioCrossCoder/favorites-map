89cd461de2aa09f5cc848409724c0b30a2004f55
BioCrossCoder <viscaria_dignics@163.com>
2025-01-25 22:43:46 +0800

    [fix] #21 avoid name change missing

--- src/entrypoints/popup/components/Graph.vue
+++ src/entrypoints/popup/components/Graph.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-import { useRouter } from 'vue-router';
+import { useRoute, useRouter } from 'vue-router';
 import { createGraphConfig } from '@/composables/config';
 import { Search } from '@element-plus/icons-vue';
 import LayoutMain from '@/components/LayoutMain.vue';
@@ -15,6 +15,15 @@ const { hoverNode, nodes, edges, eventHandlers } = buildSelectGraph(data);
 const totalCount = computed<number>(() => data.value.length);
 const selectedCount = computed<number>(() => selectedNodes.value.length);
 const router = useRouter();
+const route = useRoute();
+function handleClickOK() {
+    router.replace({
+        path: '/',
+        query: {
+            name: route.query.name,
+        },
+    })
+}
 </script>
 
 <template>
@@ -22,7 +31,7 @@ const router = useRouter();
         <el-header class="header">
             <el-row>
                 <el-col :span="8">
-                    <el-button type="primary" @click="router.back">OK</el-button>
+                    <el-button type="primary" @click="handleClickOK">OK</el-button>
                     <el-button @click="handleClickReset">Reset</el-button>
                 </el-col>
                 <el-col :span="8">
